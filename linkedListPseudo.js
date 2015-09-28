d68c5a6fbb852fd44bc852d936d8d6e72cfd8ea8
Anna Rogers <2muddybarefeet@gmail.com>
2015-09-28 08:26:02 -0700

    Added Recusive Solutions to Linked List

--- linkedListPseudo.js
+++ linkedListPseudo.js
@@ -26,7 +26,7 @@ var List = function() {
      this.end.next = new MakeNode(data); //the end of the list.next key set to new node
      this.end = this.end.next; //set end to be the end of this new node added
     }
-
+    return this;
   };
 
 
@@ -50,6 +50,42 @@ var List = function() {
         current = current.next;
       }
       return false;
+  };
+
+  //recursive versions of removeTail and contains!
+  List.prototype.removeTailRecursively = function() {
+    var current = this.start;
+    var nextData = current.next;
+    var that = this;
+    var inner = function(current,nextData) {
+      if(nextData.next === null) {
+        current.next = null; //once looped to the end set one before the ends next to be null
+        that.end = current; //set end value to be the current/second last thing
+      } else {
+        current = nextData; //set the current to the next thing in the list 
+        nextData = nextData.next; //set the next data to the next thing in the list
+        inner(current,nextData);
+      }
+    };
+    inner(current,nextData);
+    return this;
+  };
+
+  List.prototype.containsRecursively = function(searchVal) {
+    var current = this.start;
+    var cont = false;
+    var inner = function(current) {
+      if(current !== null) {
+        if(current.data === searchVal) {
+          cont = true;
+        } else {
+          current = current.next;
+          inner(current);
+        }
+      }
+    };
+    inner(current);
+    return cont;
   };
 
 var roo = new List();
